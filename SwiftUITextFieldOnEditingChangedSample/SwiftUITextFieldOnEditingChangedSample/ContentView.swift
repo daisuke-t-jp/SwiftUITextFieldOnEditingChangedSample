2e82e604b8760b24fc899c585563aec5bbacdad4
Daisuke TONOSAKI <daisuke.t.jp@gmail.com>
2019-11-26 22:30:29 +0900

    Add empty project

---
+++ SwiftUITextFieldOnEditingChangedSample/SwiftUITextFieldOnEditingChangedSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITextFieldOnEditingChangedSample
+//
+//  Created by Daisuke TONOSAKI on 2019/11/26.
+//  Copyright © 2019 Daisuke TONOSAKI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
